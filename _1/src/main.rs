92ade12333127cf2ee1ff4f55733ac25fe2fcb9d
Ibrahim <taibibrahim2001@gmail.com>
2024-12-01 23:02:37 +0100

    second exercice

--- _1/src/main.rs
+++ _1/src/main.rs
@@ -1,9 +1,15 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::iter::zip;
 use std::vec;
 
 fn main() {
+    println!("Solution to question number 1: {}", ex1());
+    println!("Solution to question number 2: {}", ex2());
+} 
+
+fn ex1() -> i32 {
     let file = File::open("input.txt").unwrap();
     let reader = BufReader::new(file);
 
@@ -20,5 +26,20 @@ fn main() {
     list1.sort();
     list2.sort();
 
-    println!("{}", zip(list1, list2).fold(0, |acc, (a, b)| acc +  (a - b).abs()));
-} 
+    zip(list1, list2).fold(0, |acc, (a, b)| acc +  (a - b).abs())
+}
+
+fn ex2() -> i32{
+    let mut counter: HashMap<i32, (i32, i32)> = HashMap::new();
+    let file = File::open("input.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let numbers: Vec<i32> = line.unwrap().split_whitespace().map(|x| x.parse::<i32>().unwrap()).collect();
+
+        counter.entry(numbers[0]).or_insert((0, 0)).0 +=1;
+        counter.entry(numbers[1]).or_insert((0, 0)).1 +=1;
+    }
+
+    counter.into_iter().fold(0, |acc, (key, (left, right))| key*left*right + acc)
+}
